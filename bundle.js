d3637038d4aec504c6ee5ccb52161694b9df7e52
Jonathon Herbert <jonathon_herbert@34841.gnm.int>
2018-12-18 18:53:09 +0000

    Comments; remove eventEmitter

--- bundle.js
+++ bundle.js
@@ -21636,54 +21636,11 @@ const regexAdapter = (input) => __awaiter(undefined, void 0, void 0, function* (
 
 //# sourceMappingURL=regex.js.map
 
-class EventEmitter {
-    constructor() {
-        this.events = {};
-    }
-    on(event, listener) {
-        if (typeof this.events[event] !== "object") {
-            this.events[event] = [];
-        }
-        this.events[event].push(listener);
-        return () => this.removeListener(event, listener);
-    }
-    removeListener(event, listener) {
-        if (typeof this.events[event] !== "object") {
-            return;
-        }
-        const idx = this.events[event].indexOf(listener);
-        if (idx > -1) {
-            this.events[event].splice(idx, 1);
-        }
-    }
-    removeAllListeners() {
-        Object.keys(this.events).forEach((event) => this.events[event].splice(0, this.events[event].length));
-    }
-    emit(event, ...args) {
-        if (typeof this.events[event] !== "object") {
-            return;
-        }
-        [...this.events[event]].forEach(listener => listener.apply(this, args));
-    }
-    once(event, listener) {
-        const remove = this.on(event, (...args) => {
-            remove();
-            listener.apply(this, args);
-        });
-        return remove;
-    }
-}
-//# sourceMappingURL=EventEmitter.js.map
-
-class ValidationService extends EventEmitter {
+class ValidationService {
     constructor(store, commands, adapter) {
-        super();
         this.store = store;
         this.commands = commands;
         this.adapter = adapter;
-        this.cancelValidation = () => {
-            this.cancelValidation();
-        };
         this.handleError = (validationInput, id, message) => {
             this.commands.applyValidationError({
                 validationInput,
