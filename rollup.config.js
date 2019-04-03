87654f5d327663b7b67386e0d8c2789dafc529de
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-04-03 14:53:25 +0100

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,30 @@ import scss from "rollup-plugin-scss";
 import babel from "rollup-plugin-babel";
 import typescript from "rollup-plugin-typescript";
 
+const plugins = [
+  typescript(),
+  scss({
+    output: "dist/index.css"
+  }),
+  babel()
+];
+
 export default [
   {
     input: "src/ts/index.ts",
     output: {
-      file: "dist/validation.js",
+      file: "dist/index.js",
+      format: "cjs"
+    },
+    plugins
+  },
+  {
+    input: "src/ts/index.ts",
+    output: {
+      file: "dist/index.m.js",
       format: "es"
     },
-    plugins: [
-      typescript(),
-      scss({
-        output: "dist/validation.css"
-      }),
-      babel()
-    ]
+    plugins
   },
   {
     // Github pages
