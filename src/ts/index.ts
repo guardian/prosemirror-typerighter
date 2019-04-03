c3e0d2dfb671f29589630c1a41298ac08bbab283
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-04-03 09:16:23 +0100

    Rebase from jsh/add-index

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,7 @@
+import "../css/validation.scss";
+import "../css/sidebar.scss";
+import "../css/validationControls.scss";
+import "../css/validationSidebarOutput.scss";
 import createValidationPlugin from "./createValidationPlugin";
 import ValidationService from "./services/ValidationAPIService";
 import createTyperighterAdapter from "./services/adapters/typerighter";
