86d09b019449580ff1703a7cd5c738be8aae534a
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-06-12 09:58:26 +0100

    Moved validations button out of toggle area; minor styling improvements

--- src/ts/components/ValidationControls.tsx
+++ src/ts/components/ValidationControls.tsx
@@ -28,16 +28,17 @@ class ValidationControls extends Component<IProps, IState> {
 
   public render() {
     const { setDebugState, setValidateOnModifyState } = this.props;
-    const { debug = false, validateOnModify = false } = this.state.pluginState || {};
+    const { debug = false, validateOnModify = false } =
+      this.state.pluginState || {};
     const { isOpen } = this.state;
     return (
       <div className="Sidebar__section">
         <div
-          className="Sidebar__header"
-          style={{ borderBottom: isOpen ? "" : "none" }}
+          className="Sidebar__header Sidebar__header-toggle"
           onClick={this.toggleOpenState}
         >
           Controls
+          <div className="Sidebar__toggle-label">Show more</div>
           <div
             className="Sidebar__toggle"
             style={{ transform: isOpen ? "" : "rotate(-90deg)" }}
@@ -45,41 +46,43 @@ class ValidationControls extends Component<IProps, IState> {
             ▼
           </div>
         </div>
-        {isOpen && (
-          <div className="Sidebar__content">
-            <div className="ValidationControls__row">
-              <label className="ValidationControls__label">
-                Validate when the document is modified
-              </label>
-              <div class="ValidationControls__input">
-                <input
-                  type="checkbox"
-                  checked={validateOnModify}
-                  className="Input"
-                  onClick={() => setValidateOnModifyState(!validateOnModify)}
-                />
+        <div className="Sidebar__content">
+          {isOpen && (
+            <div>
+              <div className="ValidationControls__row">
+                <label className="ValidationControls__label">
+                  Validate when the document is modified
+                </label>
+                <div class="ValidationControls__input">
+                  <input
+                    type="checkbox"
+                    checked={validateOnModify}
+                    className="Input"
+                    onClick={() => setValidateOnModifyState(!validateOnModify)}
+                  />
+                </div>
+              </div>
+              <div className="ValidationControls__row">
+                <label className="ValidationControls__label">
+                  Show dirty and pending ranges
+                </label>
+                <div class="ValidationControls__input">
+                  <input
+                    type="checkbox"
+                    checked={debug}
+                    className="Input"
+                    onClick={() => setDebugState(!debug)}
+                  />
+                </div>
               </div>
             </div>
-            <div className="ValidationControls__row">
-              <label className="ValidationControls__label">
-                Show dirty and pending ranges
-              </label>
-              <div class="ValidationControls__input">
-                <input
-                  type="checkbox"
-                  checked={debug}
-                  className="Input"
-                  onClick={() => setDebugState(!debug)}
-                />
-              </div>
-            </div>
-            <div className="ValidationControls__row">
-              <button className="Button" onClick={this.props.validateDocument}>
-                Validate whole document
-              </button>
-            </div>
+          )}
+          <div className="ValidationControls__row">
+            <button className="Button" onClick={this.props.validateDocument}>
+              Validate whole document
+            </button>
           </div>
-        )}
+        </div>
       </div>
     );
   }
