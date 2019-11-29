9c08a8ce64858f62767b4a5741c2ac8892b9ebbd
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-11-29 09:18:28 +0000

    Insert character for non-text leaf nodes when transforming document blocks

--- src/ts/utils/block.ts
+++ src/ts/utils/block.ts
@@ -2,7 +2,11 @@ import { Transaction } from "prosemirror-state";
 import { IRange, IBlock } from "../interfaces/IMatch";
 
 export const createBlock = (tr: Transaction, range: IRange): IBlock => {
-  const text = tr.doc.textBetween(range.from, range.to);
+  // Carriage returns are removed by textBetween, but they're one character
+  // long, so if we strip them any position beyond them will be incorrectly offset.
+  // The final argument of 'textBetween' here adds a newline character to represent
+  // a non-text leaf node.
+  const text = tr.doc.textBetween(range.from, range.to, undefined, "\n");
   return {
     text,
     ...range,
