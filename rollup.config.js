990fc13ccb4cc689a82120ac20d932c0ccf79fb4
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-05-29 17:54:07 +0100

    Update rollup deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import scss from "rollup-plugin-scss";
 import babel from "@rollup/plugin-babel";
-import typescript from "rollup-plugin-typescript2";
+import typescript from '@rollup/plugin-typescript';
 
 const plugins = [
   typescript(),
