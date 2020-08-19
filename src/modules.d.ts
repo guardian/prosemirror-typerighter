fa6a57bf0e82b7ecc54cd0e3751fcb36d13214dc
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-08-19 09:23:04 +0100

    Add declaration for snarkdown, as it's not yet in the npm release

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,3 +1,11 @@
 declare module "prosemirror-test-builder";
 declare module "prosemirror-example-setup";
+// Taken from https://github.com/developit/snarkdown/blob/master/snarkdown.d.ts –
+// at time of writing the typescript definition file is not yet in the npm release.
+declare module "snarkdown" {
+  interface Links {
+    [index: string]: string;
+  }
+  export default function (urlStr: string, prevLinks?: Links): string;
+}
 declare module "*.scss";
