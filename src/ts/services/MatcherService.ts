6ff68fdf702596d9edea07f2a65d988e95b6404f
Rhys Mills <rhystmills@gmail.com>
2022-08-12 09:19:44 +0100

    Restore console warn but make it useful

--- src/ts/services/MatcherService.ts
+++ src/ts/services/MatcherService.ts
@@ -43,6 +43,9 @@ class MatcherService<TFilterState, TMatch extends IMatch> {
     });
   }
   public getCommands = () => {
+    if (!this.commands){
+      console.warn("[prosemirror-typerighter] Attempted to use commands before they were available")
+    }
     return this.commands
   }
 
