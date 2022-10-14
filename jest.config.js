ea2b272d1c915551c7c0db8fa9984eb272ef7415
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-10-14 10:11:08 +0100

    Bump Jest to latest to remove test warnings

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
-  // collectCoverage: true,
   coverageReporters: ["json", "html"],
-  setupFiles: ['./.jest.setup.js'],
-  globals: {
-    "ts-jest": {
-      isolatedModules: true
-    }
+  setupFiles: ["./.jest.setup.js"],
+  moduleNameMapper: {
+    // uuid resolves to an ES6 module, which Node does not yet support
+    "^uuid$": require.resolve("uuid")
   }
 };
